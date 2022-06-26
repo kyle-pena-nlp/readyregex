eaa5367b131f7ed8cc3631027913bc9935ec1dbc
Sarah <strange1120@gmail.com>
2022-06-26 12:25:12 -0700

    first pass at implementing github action workflows

--- readyregex/object_model/type_hint_validation_mixin.py
+++ readyregex/object_model/type_hint_validation_mixin.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass, fields
-import typeguard
 from ..ready_regex_exception import ReadyRegexException
+
+
+try:
+    # This library is installed in test / dev builds
+    import typeguard
+except:
+    # We fake out typeguard to support zero-dependency installs in non-dev / non-test builds
+    class typeguard:
+        @staticmethod
+        def check_type(field_name, attr, field_type):
+            return True
 
 @dataclass
 class TypeHintValidationMixin:
--- setup.py
+++ setup.py
@@ -4,17 +4,13 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst', encoding="utf8") as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst', encoding="utf8") as history_file:
-    history = history_file.read()
-
-requirements = [ 'typeguard >= 2.11.1' ]
-
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest>=3', ]
+with open("test-requirements.txt") as f:
+    tests_require = [ req.strip() for req in f.readlines() ]
+
+with open("requirements.txt") as f:
+    install_requires = [ req.strip() for req in f.readlines() ]
 
 setup(
     author="Kyle Alexander Pena",
@@ -32,16 +28,15 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     description="readyregex contains a wide range of customizable and ready-made regex patterns for everyday use",
-    install_requires=requirements,
+    install_requires=install_requires,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='readyregex',
     name='readyregex',
     packages=find_packages(include=['readyregex', 'readyregex.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=tests_require,
     url='https://github.com/kyle-pena-nlp/readyregex',
     version='0.0.1',
     zip_safe=False,
