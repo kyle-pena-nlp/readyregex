62701ba8fed43561187dfc6d137baba949754676
Sarah <strange1120@gmail.com>
2022-06-26 13:02:21 -0700

    removed py35 and py36 because pytest is not supported on those versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup, find_packages
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = []
 
 with open("requirements_dev.txt") as f:
     tests_require = [ req.strip() for req in f.readlines() ]
@@ -15,17 +15,24 @@ with open("requirements.txt") as f:
 setup(
     author="Kyle Alexander Pena",
     author_email='kp1197@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3 :: Only",
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',   
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing',
+        'Topic :: Utilities' 
     ],
     description="readyregex contains a wide range of customizable and ready-made regex patterns for everyday use",
     install_requires=install_requires,
@@ -33,7 +40,7 @@ setup(
     include_package_data=True,
     keywords='readyregex',
     name='readyregex',
-    packages=find_packages(include=['readyregex', 'readyregex.*']),
+    packages=find_packages(exclude=['tests']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=tests_require,
